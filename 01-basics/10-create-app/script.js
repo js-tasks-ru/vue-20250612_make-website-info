fb1cb89c1be407f485c20c5ed0e37096936c1198
Gennady <make.website.info@gmail.com>
2025-06-20 07:25:41 +0300

    10-create-app

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,36 @@
 import { defineComponent, createApp } from 'vue'
+
+const App = defineComponent({
+  name: 'App',
+
+  // setup - сборка экземпляра
+  // Для сборки — настройки и определения свойств экземпляра компонента – в Composition API используется функция setup
+  setup() {
+    function formatAsLocalDate() {
+      return new Date().toLocaleString(navigator.language, {
+      // return new Date().toLocaleString('ru', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      })
+    }
+    return {
+      formatAsLocalDate
+    }
+  },
+
+  template: `
+    <div>Сегодня {{ formatAsLocalDate() }}</div>
+  `,
+});
+
+// Для создания Vue приложения (Vue Application, Vue App) из корневого компонента
+const app = createApp(App);
+
+// Монтирование приложения на страницу
+const vm = app.mount('#app');
+// vm - экземпляр компонента App
+
+// Для отладки - добавим в глобальную переменную
+// Позже научимся делать это через vue-devtools
+window.vm = vm;
