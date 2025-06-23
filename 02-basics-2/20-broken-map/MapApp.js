907aa8ae35a171db25e02d9df6536cb7fc73a9bf
Gennady <make.website.info@gmail.com>
2025-06-23 17:10:30 +0300

    Solve Map App

--- 02-basics-2/20-broken-map/MapApp.js
+++ 02-basics-2/20-broken-map/MapApp.js
@@ -1,4 +1,4 @@
-import { defineComponent, ref, watch } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'MapApp',
@@ -13,27 +13,41 @@ export default defineComponent({
      * @param {MouseEvent} event
      */
     function handleClick(event) {
-      x = event.offsetX
-      y = event.offsetY
+      x.value = event.offsetX
+      y.value = event.offsetY
     }
 
     // Следим за X и Y для установки нового положения
-    watch([x, y], () => {
-      // Находим метку и изменяем её положение
-      const map = document.querySelector('.pin')
-      map.style.left = `${x}px`
-      map.style.top = `${y}px`
-    })
+    // ❗ В Vue за редкими исключениями нельзя манипулировать DOM напрямую.
+    // watch([x, y], () => {
+    //   console.log('x = ', x.value);
+    //   console.log('y = ', y.value);
+    //
+    //   // Находим метку и изменяем её положение
+    //   const map = document.querySelector('.pin')
+    //
+    //   console.log('map - ', map);
+    //
+    //   map.style.left = `${x.value}px`
+    //   map.style.top = `${y.value}px`
+    // })
 
     return {
       handleClick,
+      x,
+      y
     }
   },
 
   template: `
     <div class="map" @click="handleClick">
       <img class="map-image" src="./map.png" alt="Map" draggable="false" />
-      <span class="pin">📍</span>
+      <span class="pin"
+      :style="{
+        left: \`\${x}px\`,
+        top: \`\${y}px\`,
+      }"
+      >📍</span>
     </div>
   `,
 })
